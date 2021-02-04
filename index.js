0c7234292421c946e6da869c003535aa80148c79
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-02-04 17:03:55 +0300

    upload

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const btn = document.getElementById('btn');
-// btn.addEventListener('click', function() {
-
-//     const cityName = document.getElementById('input').value;
-//     const api = "938356ce0f30dc169e4b8941b4bf09a1";
-//     const area = document.getElementById('area')
-//     area.innerText = cityName
-//     getWeather(cityName,api,area);
-// })
-
-
-// function getWeather(cityName,api,area) {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${api}`)
-//     .then(res => res.json())
-//     .then(data => {
-
-//         console.log(area)
-//         const celcious = document.getElementById('celcius')
-//         const weather = document.getElementById('weather')
-//         celcious.innerText = data.main.temp
-//         weather.innerText = weather[0].main
-//         console.log(data)
-//     })
-// }
-
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', function() {
